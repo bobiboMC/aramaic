870a6fd8655ea250b2dc7de2f6690ad3631e5b6e
MichaelBeini <bobiboanhel@gmail.com>
2024-03-24 15:31:52 +0200

    Updates

--- server/index.js
+++ server/index.js
@@ -181,7 +181,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "70ef93"
+  version_hash: "11m74qd"
 };
 async function get_hooks() {
   return {};
